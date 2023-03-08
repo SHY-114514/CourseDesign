4bdd1cbd394bcd3b4717f7adcacd04023df29e6b
SHY-114514 <93690269+SHY-114514@users.noreply.github.com>
2023-03-08 18:47:53 +0800

    Delete resources directory

--- resources/js/index.js
+++
@@ -1,7 +0,0 @@
-function userInfo(uid){
-    const Http = new XMLHttpRequest();
-    const url='https://jsonplaceholder.typicode.com/posts';
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange = (e) => { console.log(Http.responseText) }
-}
