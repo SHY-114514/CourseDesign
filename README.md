550b7897915bf3ca920630d3fd1c01c9fce93fe1
SHY <sun_1917121416@163.com>
2023-03-08 21:24:03 +0800

    修改登录BUG

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# CourseDesign
+# 课程设计
 
 
 ### 项目结构
 
 /main.cpp 项目主要代码
-
-/server.cpp 若是时间足够，将项目分为S/C（Server端和Client端）
 
 /data/ 存放数据文件
 
--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@
         scanf("%d", &flag);                            \
     }
 
+int LOGINCNT = 1;
+
 typedef struct
 {
     long id;              // 主键
@@ -56,7 +58,8 @@ typedef struct
     char role;       // 0：普通用户（默认）   1：管理员
 } Account;           // 账号
 
-typedef union{
+typedef union
+{
     User user;
     Account account;
 } Data;
@@ -68,91 +71,183 @@ typedef struct _LinkedList
     struct _LinkedList *next;
 } Node, *List; // 链表结构
 
-int ListInit(List &l,int type){
+int ListInit(List &l, int type)
+{
     l = new Node();
-    if(!l)  return FALSE;
+    if (!l)
+        return FALSE;
     l->next = NULL;
     l->type = type;
     return TRUE;
 }
-int ListInsert(List &l,Data &data){
-    Node* np = new Node();
-    if(!np) return FALSE;
-    if(l->type == DTTPACT){
+int ListInsert(List &l, Data &data)
+{
+    Node *np = new Node();
+    if (!np)
+        return FALSE;
+    if (l->type == DTTPACT)
+    {
         np->data.account.id = data.account.id;
-        strcpy(np->data.account.mail,data.account.mail);
-        strcpy(np->data.account.passwd,data.account.passwd);
+        strcpy(np->data.account.mail, data.account.mail);
+        strcpy(np->data.account.passwd, data.account.passwd);
         np->data.account.role = data.account.role;
-    }else if(l->type == DTTPUSR){
+    }
+    else if (l->type == DTTPUSR)
+    {
         np->data.user.uid = data.user.uid;
-        strcpy(np->data.user.name,data.user.name);
+        strcpy(np->data.user.name, data.user.name);
         np->data.user.age = data.user.age;
         np->data.user.gender = data.user.gender;
-        strcpy(np->data.user.phone,data.user.phone);
-        strcpy(np->data.user.disease,data.user.disease);
-        strcpy(np->data.user.allergy,data.user.allergy);
+        strcpy(np->data.user.phone, data.user.phone);
+        strcpy(np->data.user.disease, data.user.disease);
+        strcpy(np->data.user.allergy, data.user.allergy);
     }
     np->next = l->next;
     l->next = np;
     return TRUE;
 }
-void ListPrint(Data data,int type){
-    if(type == DTTPACT){
+int ListLocate(List &l, Data *dp)
+{
+    List p = l->next;
+    while (p)
+    {
+        if (l->type == DTTPACT)
+            if (p->data.account.id == dp->account.id || strcmp(p->data.account.mail, dp->account.mail) == 0)
+            {
+                dp->account.id = p->data.account.id;
+                strcpy(dp->account.mail, p->data.account.mail);
+                strcpy(dp->account.passwd, p->data.account.passwd);
+                dp->account.role = p->data.account.role;
+                return TRUE;
+            }
+            else if (l->type == DTTPUSR)
+            {
+                if (p->data.user.uid == dp->user.uid)
+                {
+                    dp->user.uid = p->data.user.uid;
+                    strcpy(dp->user.name, p->data.user.name);
+                    dp->user.age = p->data.user.age;
+                    dp->user.gender = p->data.user.gender;
+                    strcpy(dp->user.phone, p->data.user.phone);
+                    strcpy(dp->user.disease, p->data.user.disease);
+                    strcpy(dp->user.allergy, p->data.user.allergy);
+                }
+            }
+        p = p->next;
+    }
+    return FALSE;
+}
+void ListPrint(Data data, int type)
+{
+    if (type == DTTPACT)
+    {
         printf("ID:%ld\t权限:%d\n邮箱:%s\n密码:%s\n", data.account.id, data.account.role, data.account.mail, data.account.passwd);
         printf("**************************\n");
     }
+    else if (type == DTTPUSR)
+    {
+        printf("UID:%d\t姓名:%s\t年龄:%d\t性别:%c\n手机号:%s\n历史疾病:%s\n过敏信息:%s\n", data.user.uid, data.user.name, data.user.age, data.user.gender, data.user.phone, data.user.disease, data.user.allergy);
+        printf("**************************\n");
+    }
 }
-void ListPrints(List &l){
+void ListPrints(List &l)
+{
     List p = l->next;
-    while(p){
-        if(l->type == DTTPACT){
-            printf("ID:%ld\t权限:%d\n邮箱:%s\n密码:%s\n",p->data.account.id,p->data.account.role,p->data.account.mail,p->data.account.passwd);
+    while (p)
+    {
+        if (l->type == DTTPACT)
+        {
+            printf("ID:%ld\t权限:%d\n邮箱:%s\n密码:%s\n", p->data.account.id, p->data.account.role, p->data.account.mail, p->data.account.passwd);
             printf("**************************\n");
-        }else if(l->type == DTTPUSR){
+        }
+        else if (l->type == DTTPUSR)
+        {
             printf("UID:%d\t姓名:%s\t年龄:%d\t性别:%c\n手机号:%s\n历史疾病:%s\n过敏信息:%s\n", p->data.user.uid, p->data.user.name, p->data.user.age, p->data.user.gender, p->data.user.phone, p->data.user.disease, p->data.user.allergy);
             printf("**************************\n");
         }
         p = p->next;
     }
 }
-int ListDelete(List &l,Data data){
-    List p = l->next,q;
-    while(p->next){
-        if(l->type == DTTPACT)
-            if(p->next->data.account.id == data.account.id || strcmp(p->next->data.account.mail,data.account.mail))
+int ListDelete(List &l, Data data)
+{
+    List p = l->next, q;
+    while (p->next)
+    {
+        if (l->type == DTTPACT)
+            if (p->next->data.account.id == data.account.id || strcmp(p->next->data.account.mail, data.account.mail))
                 break;
         p = p->next;
     }
-    if(!p) return FALSE;
+    if (!p)
+        return FALSE;
     q = p->next;
     p->next = q->next;
     delete q;
     return TRUE;
 }
-void ListClear(List &l){
-    List p = l->next,q;
-    while(p){
+void ListClear(List &l)
+{
+    List p = l->next, q;
+    while (p)
+    {
         q = p;
         p = p->next;
         delete q;
     }
 }
 
+int login(List &accounts)
+{
+    if (LOGINCNT > 3)
+        return FALSE;
+    char msg[21];
+    Data data;
+    data.account.id = 0;
+    List p = accounts->next;
+    printf("请输入账号:");
+    scanf("%s", data.account.mail);
+    if (!ListLocate(accounts, &data))
+    {
+        sprintf(msg, "账号不存在(%d/3)", LOGINCNT);
+        LOGW(msg)
+        LOGINCNT += 1;
+        return login(accounts);
+    }
+    printf("请输入密码:");
+    scanf("%s", msg);
+    if (strcmp(data.account.passwd, msg) != 0)
+    {
+        sprintf(msg, "密码错误(%d/3)", LOGINCNT);
+        LOGW(msg)
+        LOGINCNT += 1;
+        return login(accounts);
+    }
+    LOGV("登录成功")
+    return data.account.id;
+}
+
 int main(void)
 {
-    List accounts,users;
-    if(!ListInit(accounts,DTTPACT)) LOGE("账号列表初始化失败");
-    if(!ListInit(users,DTTPUSR)) LOGE("用户列表初始化失败");
-    FILE *fp = fopen(ACCOUNTDATA,"rb");
-    if(!fp) LOGE("账号文件打开失败");
-    while(!feof(fp)){
+    int option = FALSE, quit = FALSE;
+    LOGV("初始化")
+    List accounts, users;
+    if (!ListInit(accounts, DTTPACT))
+        LOGE("账号列表初始化失败")
+    if (!ListInit(users, DTTPUSR))
+        LOGE("用户列表初始化失败")
+    FILE *fp = fopen(ACCOUNTDATA, "rb");
+    if (!fp)
+        LOGE("账号文件打开失败");
+    while (!feof(fp))
+    {
         Data data;
         fscanf(fp, "%d %s %s %c\n", &(data.account.id), data.account.mail, data.account.passwd, &(data.account.role));
-        ListInsert(accounts,data);
+        ListInsert(accounts, data);
     }
     fclose(fp);
-    fp = fopen(USERDATA,"rb");
-    if(!fp) LOGE("用户文件打开失败");
+    fp = fopen(USERDATA, "rb");
+    if (!fp)
+        LOGE("用户文件打开失败")
     while (!feof(fp))
     {
         Data data;
@@ -161,11 +256,26 @@ int main(void)
     }
     fclose(fp);
 
+    Data _dt;
+    _dt.user.uid = login(accounts);
+    if(_dt.user.uid == FALSE) quit = TRUE;
+    ListLocate(users, &_dt);
+    while (!quit)
+    {
+        printf("【0】退出系统\t【1】查看账户列表\n");
+        PRINT(_dt.user.name, option);
+        switch (option)
+        {
+        case 0:
+            quit = TRUE;
+            break;
+        case 1:
+            ListPrints(accounts);
+            break;
+        }
+    }
 
-    ListPrints(accounts);
-    ListPrints(users);
-
-
+    LOGV("退出");
     ListClear(accounts);
     ListClear(users);
 
